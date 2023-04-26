e99e1cce3764a59a46698184d5078c804215eb38
ayush-py-dev <ayush-py-dev@users.noreply.github.com>
2023-04-26 09:34:37 +0000

    Format code with black

--- cogs/music.py
+++ cogs/music.py
@@ -69,9 +69,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
             description=f"```Added {data['title']} to the Queue```",
             color=Color.blurple(),
         ).set_footer(text=f"Requested by {str(ctx.author)}")
-        await ctx.reply(
-            embed = embed
-        )
+        await ctx.reply(embed=embed)
 
         if download:
             source = ytdl.prepare_filename(data)
