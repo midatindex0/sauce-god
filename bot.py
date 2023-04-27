e28ddbdcbede6673e469f62491736a6010db035d
Lonely-code-cube <lonely-code-cube@github.com>
2023-04-27 13:30:28 +0530

    fixed owner role issue

--- bot.py
+++ bot.py
@@ -24,7 +24,6 @@ class BaseBot(commands.Bot):
             super().__init__(
                 intents=discord.Intents.all(),
                 command_prefix=config["default"]["prefix"],
-                owner_ids=config["default"]["owner_ids"] * args,
                 **kwargs,
             )
             log_handler = logging.FileHandler(
