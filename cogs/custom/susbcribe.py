154c0f5741391896842e5b0d6ee731ad5e09e2d5
ayush-py-dev <clashofserver8823@gmail.com>
2023-05-10 09:25:06 +0530

    started working on subscribe command

---
+++ cogs/custom/susbcribe.py
@@ -0,0 +1,7 @@
+import discord
+from discord.ext import commands
+
+import core.utils as utils
+
+class Subscribe(commands.Cog):
+    ...
--- core/utils.py
+++ core/utils.py
@@ -1,6 +1,9 @@
 import tomli
-
+from pytimeparse import parse
 
 def read_config():
     f = open("config.toml", "rb")
     return tomli.load(f)
+
+def _parse(time: str) -> int:
+    return int(parse(time))
